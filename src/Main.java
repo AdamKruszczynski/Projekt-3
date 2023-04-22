7501e934017d07a38279dcf82d76ffd97c236d0d
AdamKruszczynski <ag27d3a6@gmail.com>
2023-04-22 12:50:01 +0200

    Projekt-3-1 1. Przerzutowanie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer a = 12;
+        metoda(a);
     }
     public static void metoda(Number number){
         if (number instanceof Integer){
