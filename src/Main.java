e413790f324fe9fd8531e97627bdf5c2d9b47a17
AdamKruszczynski <ag27d3a6@gmail.com>
2023-04-22 12:33:31 +0200

    Projekt-3-1 1. Przerzutowanie

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void metoda(Number number){
+        if (number instanceof Integer){
+            System.out.println("Liczba jest typu: Integer");
+        } else if (number instanceof  Double) {
+            System.out.println("Liczba jest typu: Double");
+        } else {
+            System.out.println("Nieoczekiwna liczba! Zmień jej typ i spróbuj ponowanie!");
+        }
+
+
+    }
 }
