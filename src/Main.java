2b2f71c620b49d4f24f28b80ae564d47f840a54c
AdamKruszczynski <ag27d3a6@gmail.com>
2023-04-23 12:22:50 +0200

    Projekt-3-2 2. Zabazpieczenie

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ public class Main {
 
         Integer a = 12;
         metoda(a);
+
+        dzielenie(10,2);
     }
+
+    public static void dzielenie(int a, int b){
+        System.out.println("Wynikiem dzielenia jest: " + a/b);
+    }
+
+
+
     public static void metoda(Number number){
         if (number instanceof Integer){
             System.out.println("Liczba jest typu: Integer");
