266fcc6b6e742de91063f068d68dd007a5fa0413
AdamKruszczynski <ag27d3a6@gmail.com>
2023-05-01 12:38:51 +0200

    Projekt-3-2 2. Zabazpieczenie

---
+++ src/LosowaLiczba.java
@@ -0,0 +1,4 @@
+public interface LosowaLiczba {
+     int losujLiczbe();
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,47 @@
-public class Main {
-    public static void main(String[] args)  {
+import java.util.Random;
 
-        Integer a = 12;
-        metoda(a);
+public class Main{
+    public static void main(String[] args) {
+        Random random = new Random();
+        LosowaLiczba liczba = () -> {
+                int i = random.nextInt(100);
+                System.out.println(i);
+                return i;
+        };
+
+        metoda(liczba.losujLiczbe());
 
         try{
-            dzielenie(10,0);
+            int a = liczba.losujLiczbe();
+            int b = 2;
+
+            Runnable runnable = () -> System.out.println("A: " +  a + ", B: " + b);
+
+            runnable.run();
+            dzielenie(a, b, 1);
+
         }catch (DzieleniePrzezZero e){
             System.out.println("Dzielenie przez zero: " + e);
         }
     }
 
-    public static void dzielenie(int a, int b) throws DzieleniePrzezZero{
-        if (b != 0) {
-            System.out.println("Wynikiem dzielenia jest: " + a / b);
-        } else {
-            throw new DzieleniePrzezZero("nie prawidłowe dane");
+    public static <Integer> void dzielenie(int a, int b, Integer c) throws DzieleniePrzezZero{
+        if (c.equals(1)) {
+            if (b != 0) {
+                System.out.println("Wynikiem dzielenia jest: " + a / b);
+            } else {
+                throw new DzieleniePrzezZero("nie prawidłowe dane");
+            }
+        } else if (c.equals(2)) {
+                System.out.println("Wynikiem mnożenia jest: " + a * b);
+        }else if (c.equals(3)) {
+            System.out.println("Wynikiem dodawania jest: " + a + b);
+        }else if (c.equals(4)) {
+            System.out.println("Wynikiem odejmowania jest: " + (a - b));
+        }else {
+            System.out.println("Podaj poprawna liczbę");
         }
     }
-
-
 
     public static void metoda(Number number){
         if (number instanceof Integer){
@@ -29,7 +51,6 @@ public class Main {
         } else {
             System.out.println("Nieoczekiwna liczba! Zmień jej typ i spróbuj ponowanie!");
         }
-
 
     }
 }
